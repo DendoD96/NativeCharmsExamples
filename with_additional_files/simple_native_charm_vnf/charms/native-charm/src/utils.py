5667980116a0588afe562402ece9d320b3357502
Daniele <daniele.rossi18@studio.unibo.it>
2021-05-29 10:48:45 +0200

    Clean up

--- with_additional_files/simple_native_charm_vnf/charms/native-charm/src/utils.py
+++ with_additional_files/simple_native_charm_vnf/charms/native-charm/src/utils.py
@@ -1,4 +1,4 @@
 def write(filename: str, text: str):
-    with open(filename, "a") as file_object:
-        file_object.write(text)
-        file_object.write("\n")
+	with open(filename, "a") as file_object:
+		file_object.write(text)
+		file_object.write("\n")
